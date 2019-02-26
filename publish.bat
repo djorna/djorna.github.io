83808fdd529f550f97793d26ee6c60f633817710
djorna <davidjorna@gmail.com>
2019-02-25 20:43:57 -0500

    Attmpt to fix css issue

--- publish.bat
+++ publish.bat
@@ -2,6 +2,6 @@ git add .
 git commit -a -m %1
 git push -u origin pelican
 pelican content -o output -s publishconf.py
-ghp-import output -r origin -b master -c davidjorna.com
+ghp-import output -r origin -b master -c "davidjorna.com"
 git push origin master
 git checkout pelican
